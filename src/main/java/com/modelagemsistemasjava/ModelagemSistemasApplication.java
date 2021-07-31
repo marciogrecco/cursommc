82af8457b3c9debe80b0011339145a98d0e40f54
Marcio Grecco <marcio.greco@outlook.com>
2021-07-31 09:02:29 -0300

    Project created

---
+++ src/main/java/com/modelagemsistemasjava/ModelagemSistemasApplication.java
@@ -0,0 +1,13 @@
+package com.modelagemsistemasjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemSistemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modelagemsistemasjava/ModelagemSistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelagemsistemasjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemSistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

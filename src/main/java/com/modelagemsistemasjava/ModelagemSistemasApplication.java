821894a7eacd43f1cb10529b5e908cb62ad9aca3
Marcio Grecco <marcio.greco@outlook.com>
2021-08-02 16:16:40 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/modelagemsistemasjava/ModelagemSistemasApplication.java
+++ src/main/java/com/modelagemsistemasjava/ModelagemSistemasApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.modelagemsistemasjava.domain.Categoria;
 import com.modelagemsistemasjava.domain.Cidade;
+import com.modelagemsistemasjava.domain.Cliente;
+import com.modelagemsistemasjava.domain.Endereco;
 import com.modelagemsistemasjava.domain.Estado;
 import com.modelagemsistemasjava.domain.Produto;
+import com.modelagemsistemasjava.enun.TipoCliente;
 import com.modelagemsistemasjava.repository.CategoriaRepository;
 import com.modelagemsistemasjava.repository.CidadeRepository;
+import com.modelagemsistemasjava.repository.ClienteRepository;
+import com.modelagemsistemasjava.repository.EnderecoRepository;
 import com.modelagemsistemasjava.repository.EstadoRepository;
 import com.modelagemsistemasjava.repository.ProdutoRepository;
 
@@ -27,26 +32,26 @@ public class ModelagemSistemasApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
-		
-				
-		Categoria cat1 = new Categoria(null,"Informatica");
-		Categoria cat2 = new Categoria(null,"Escritorio");
-		
-		
-		Produto p1 = new Produto(null,"Computador", 2000.0);
-		Produto p2 = new Produto(null,"Impressora", 850.0);
-		Produto p3 = new Produto(null,"Mouse",150.0);
-		
-		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 850.0);
+		Produto p3 = new Produto(null, "Mouse", 150.0);
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 
@@ -54,20 +59,30 @@ public class ModelagemSistemasApplication implements CommandLineRunner {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 
-		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
-		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
 	}
 

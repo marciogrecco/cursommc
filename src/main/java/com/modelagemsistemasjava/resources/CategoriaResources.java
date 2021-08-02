dc6f45dc403d456711ef16f8f8f39107676d8a3c
Marcio Grecco <marcio.greco@outlook.com>
2021-08-02 08:48:52 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/modelagemsistemasjava/resources/CategoriaResources.java
+++ src/main/java/com/modelagemsistemasjava/resources/CategoriaResources.java
@@ -1,17 +1,31 @@
 package com.modelagemsistemasjava.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.modelagemsistemasjava.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
 
-		return "Olá Spring boot";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }

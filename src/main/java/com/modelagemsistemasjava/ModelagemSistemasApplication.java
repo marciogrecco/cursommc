bd687eb724dc841ed9ccd40c9e69e94455502965
Marcio Grecco <marcio.greco@outlook.com>
2021-08-02 10:46:45 -0300

    Criando operação de instanciação

--- src/main/java/com/modelagemsistemasjava/ModelagemSistemasApplication.java
+++ src/main/java/com/modelagemsistemasjava/ModelagemSistemasApplication.java
@@ -1,13 +1,33 @@
 package com.modelagemsistemasjava;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.modelagemsistemasjava.domain.Categoria;
+import com.modelagemsistemasjava.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class ModelagemSistemasApplication {
+public class ModelagemSistemasApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemSistemasApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }

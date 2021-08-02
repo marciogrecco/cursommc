ab8abe18b1c803ea71eede6ffe6d2c363ff955ad
Marcio Grecco <marcio.greco@outlook.com>
2021-08-02 12:58:56 -0300

    Produto e categoria associação muito para muitos

--- src/main/java/com/modelagemsistemasjava/ModelagemSistemasApplication.java
+++ src/main/java/com/modelagemsistemasjava/ModelagemSistemasApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.modelagemsistemasjava.domain.Categoria;
+import com.modelagemsistemasjava.domain.Produto;
 import com.modelagemsistemasjava.repository.CategoriaRepository;
+import com.modelagemsistemasjava.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class ModelagemSistemasApplication implements CommandLineRunner {
@@ -19,15 +21,28 @@ public class ModelagemSistemasApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
+		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
-
-}
+} 
